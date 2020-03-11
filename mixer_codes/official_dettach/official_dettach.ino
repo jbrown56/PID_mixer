f4bb32969f6e2d0a916b21b51fb92d29737f9aff
Justine H Brown <jbrown56@ccs.local>
2020-03-11 10:57:17 -0400

    updated official code

--- mixer_codes/official_dettach/official_dettach.ino
+++ mixer_codes/official_dettach/official_dettach.ino
@@ -2,8 +2,8 @@
 // Detach interrupt code
 // get detach interrupt to workd with the photoreflector to measure interrupts and convert that to speed
 int photoPin1 = 2;
-//int photoPin2 = 3;
-int ledPin = 8;
+int photoPin2 = 3;
+int ledPin = 9;
 int photoState = analogRead(photoPin1);
 int interrupt = 0;
 long oldTime = 0;
@@ -17,48 +17,58 @@ void on()
 	if (toggle == 0)
 	{
 		interrupt = interrupt + 1;
-		//toggle = 1;
+		digitalWrite(ledPin, HIGH);
+		// toggle = 1;
 	}
 }
 
-/*void off()
+void off()
 {
 	toggle = 0;
-	test++;
+	digitalWrite(ledPin, LOW);
 	// Variebles are needed
-	// interrput = interrupt -1;
+	// interrupt = interrupt + 1;
 }
-*/
+
 void setup()
 {
 	Serial.begin(9600);
 	pinMode(photoPin1, INPUT);
-	//pinMode(photoPin2, INPUT_PULLUP);
+	pinMode(photoPin2, INPUT);
 	pinMode(ledPin, OUTPUT);
-	//attachInterrupt(digitalPinToInterrupt(2), off, FALLING); // Works faster than a loop. If there is nothing in the way, the light turns off
-	attachInterrupt(digitalPinToInterrupt(photoPin1), on, RISING); // Works faster than a loop. If there is something in the way, the light turns on
+	attachInterrupt(digitalPinToInterrupt(photoPin2), off, RISING); // Works faster than a loop.
+	// If there is nothing in the way, the light turns off
+	attachInterrupt(digitalPinToInterrupt(photoPin1), on, FALLING); // Works faster than a loop.
+	// If there is something in the way, the light turns on
 	// Detach, do the math, reattach
 }
 
 void loop()
 {
-	Time = millis(); //Time counts up
-	if (Time > check + oldTime) //It runs every second
+	Time = millis(); // Time counts up
+	if (Time > check + oldTime)
+	// It runs every second
 	{
-	//detachInterrupt(digitalPinToInterrupt(3));
-	detachInterrupt(digitalPinToInterrupt(2));
-	rps = interrupt/((Time - oldTime)/check); //The rate it changes is the number of interrupts divided by the amount of milliseconds passed. Dividing it by 1000 puts it in terms of seconds
-	oldTime = millis(); //Resets old time back to 0
-	interrupt = 0; //Resets the amount of interrupts
-	//attachInterrupt(digitalPinToInterrupt(3), off, FALLING);
-	attachInterrupt(digitalPinToInterrupt(2), on, RISING);
-	Serial.println("time > 1000!");
+		/*
+		Serial.println(interrupt);
+		oldTime = Time;
+		}
+		*/
+		detachInterrupt(digitalPinToInterrupt(3));
+		detachInterrupt(digitalPinToInterrupt(2));
+		rps = interrupt/ ((Time - oldTime) /check); // The rate it changes is the number of
+		// interrupts divided by the amount of milliseconds passed. Dividing it by 1000 puts it in terms of seconds
+		interrupt = 0; // Resets the amount of interrupts
+		Serial.println("time > 1000!");
+		Serial.println(rps);
+		attachInterrupt(digitalPinToInterrupt(photoPin2), off, RISING); // Works faster than a loop.
+		attachInterrupt(digitalPinToInterrupt(photoPin1), on, FALLING);
+		oldTime = millis(); // Resets old time back to 0
 	}
-	
-	//Serial.print(toggle);
-	//Serial.print("\t");
-	Serial.println(rps);
-	//Serial.print("\t");
-	//Serial.println(test);
+	// Serial.print(toggle);
+	// Serial.print("\t");
+	// Serial.println(rps);
+	// Serial.print("\t");
+	// Serial.println(test);
 	delay(100);
 }
